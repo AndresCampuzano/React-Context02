32360bd97d071470b557dd91f6f0eb7ecae61efc
Andres Campuzano <campuxano88@gmail.com>
2020-07-30 11:52:03 -0500

    Improving context.

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ContextTodo } from './Context';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ContextTodo>
+      <App />
+    </ContextTodo>
   </React.StrictMode>,
   document.getElementById('root')
 );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,14 @@ import React from 'react';
 import Header from '../components/Header.jsx';
 import Form from '../components/Form.jsx';
 import ListTodos from '../components/ListTodos.jsx';
-import { ContextTodo } from '../Context';
 
 const Home = () => {
   return (
-    <ContextTodo>
+    <>
       <Header />
       <Form />
       <ListTodos />
-    </ContextTodo>
+    </>
   );
 };
 
